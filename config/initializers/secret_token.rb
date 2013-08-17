9da2f92758844bc492e370ba527074047844ed9d
Bernhard Zuern <bernhard.zuern@googlemail.com>
2013-08-18 00:21:57 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Newsgrabber::Application.config.secret_key_base = '435d959f0577c689aa0d0135dcbe559294fa52f801b0c0915b2c3527b38a90cebc061812a933e7a071c8c82e2d21dfe512523ce08c21fdce56f4ad01d903975d'
