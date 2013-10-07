5987b70030a1cc112933fccc7a4075cf1ba84333
Bernhard Zuern <bernhard.zuern@googlemail.com>
2013-10-07 19:32:26 +0200

    Bugfix

--- app/controllers/rss_sources_controller.rb
+++ app/controllers/rss_sources_controller.rb
@@ -50,7 +50,7 @@ class RssSourcesController < ApplicationController
   # PATCH/PUT /rss_sources/1                       1
   # PATCH/PUT /rss_sources/1.json
   def update
-    respond_to do |format|d
+    respond_to do |format|
       if @rss_source.update(rss_source_params)
         format.html { redirect_to @rss_source, notice: 'Rss source was successfully updated.' }
         format.json { head :no_content }
