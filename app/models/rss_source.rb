ff0b41a27354c78e7176660fc2bf0c8da0611d9a
Bernhard Zürn <bernhard.zuern@gmail.com>
2013-12-11 21:26:23 +0100

    refs #18 - bugfix

--- app/models/rss_source.rb
+++ app/models/rss_source.rb
@@ -4,7 +4,7 @@ class RssSource < ActiveRecord::Base
 
   def update_from_feed
     feed = Feedzirra::Feed.fetch_and_parse(self.url)
-    unless feed.nil?
+    unless 0==feed
       feed.entries.each do |entry|
         guid = entry.id
         news_item = self.news_items.where(:guid => guid).first
