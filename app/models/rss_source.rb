62fe9847cfab86ea8f223ce0220ee1c6552375d2
Bernhard Zürn <bernhard.zuern@gmail.com>
2013-12-11 21:01:38 +0100

    refs #18 Bugfix - Do not loop over emtpy feed-results.

--- app/models/rss_source.rb
+++ app/models/rss_source.rb
@@ -4,19 +4,23 @@ class RssSource < ActiveRecord::Base
 
   def update_from_feed
     feed = Feedzirra::Feed.fetch_and_parse(self.url)
-    feed.entries.each do |entry|
-      guid = entry.id
-      news_item = self.news_items.where(:guid => guid).first
-      if news_item.nil?
-        self.news_items.create!(
-          :headline => entry.title,
-          :summary => entry.summary,
-          :web_url => entry.url,
-          :release_date => entry.published,
-          :guid => entry.id,
-          #:rss_source_id => self.id
-        )
+    unless feed.entries.eql? 0
+      feed.entries.each do |entry|
+        guid = entry.id
+        news_item = self.news_items.where(:guid => guid).first
+        if news_item.nil?
+          self.news_items.create!(
+            :headline => entry.title,
+            :summary => entry.summary,
+            :web_url => entry.url,
+            :release_date => entry.published,
+            :guid => entry.id,
+            #:rss_source_id => self.id
+          )
+        end
       end
     end
   end
+
+  #@TODO: .gsub(/feed:\/\//,'http://')
 end
