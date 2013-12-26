4d97197ab03c7b71329e04c9c0af2d782456da15
Bernhard Zürn <bernhard.zuern@googlemail.com>
2013-12-26 13:20:36 +0100

    Fixed seeds for cucumber-testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-if RssSource.count == 0
-  RssSource.create!(
-      :title => 'Google News  - Elektromotor',
-      :url => 'http://news.google.com/news?hl=de&gl=de&authuser=0&q=elektromotor&um=1&ie=UTF-8&output=rss'
-  )
-end
 
 if User.count == 0
   User.create!(
-      :email => "bernhard.zuern@googlemail.com",
-      :password => "demo12345",
-      :password_confirmation => "demo12345"
+      :email => 'bernhard.zuern@googlemail.com',
+      :password => 'demo12345',
+      :password_confirmation => 'demo12345'
   )
 end
+
+
+if RssSource.count == 0
+  RssSource.create!(
+      :title => 'Google News  - Elektromotor',
+      :url => 'http://news.google.com/news?hl=de&gl=de&authuser=0&q=elektromotor&um=1&ie=UTF-8&output=rss',
+      :user => User.where(:email => 'bernhard.zuern@googlemail.com').first
+  )
+end
+
