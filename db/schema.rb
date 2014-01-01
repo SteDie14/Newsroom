b6dd9fdf70a1595c0875efeaee53f5a1a0691d4d
Bernhard Zürn <bernhard.zuern@googlemail.com>
2014-01-01 16:38:56 +0100

    DB-Tabellen Incomes und Expenses per Migration entfernt -> Es gibt dafür die neue Tabelle Bookings der gleichnamigen Basismodelklasse.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140101143009) do
+ActiveRecord::Schema.define(version: 20140101144007) do
 
   create_table "accounts", force: true do |t|
     t.string   "name"
@@ -29,17 +29,6 @@ ActiveRecord::Schema.define(version: 20140101143009) do
     t.string   "type"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "expenses", force: true do |t|
-    t.string   "title"
-    t.decimal  "amount",          precision: 10, scale: 0
-    t.integer  "tax_rate"
-    t.date     "accounting_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "account_id"
-    t.integer  "user_id"
   end
 
   create_table "folders", force: true do |t|
