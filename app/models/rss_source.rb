0c5dba81aa1ae5f504ed1c4166d723fe97f27534
Bernhard Zürn <bernhard.zuern@googlemail.com>
2014-01-11 22:37:57 +0100

    Fehlerbehandlung für HTTP 500 beim Abrufen von Feeds

--- app/models/rss_source.rb
+++ app/models/rss_source.rb
@@ -16,11 +16,11 @@ class RssSource < ActiveRecord::Base
   def update_from_feed(owner)
     feed = Feedzirra::Feed.fetch_and_parse(self.url)
     unless 0==feed || feed.nil?
-
+      if feed == 500
+        return
+      end
       feed.entries.each do |entry|
-        if feed.entries == 500
-          next
-        end
+
         guid = entry.id
         news_item = self.news_items.where(:guid => guid).first
         if news_item.nil?
